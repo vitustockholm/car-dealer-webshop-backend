7511cf18fee3c14d95d50f8f1f5283dfcbf682bb
Vitus <83448449+vitustockholm@users.noreply.github.com>
2022-04-19 13:14:33 +0300

    productioning to the cloud service
    added extra MONGODB_URI for testing production deployment to the cloud service

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // Connecting DB
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGODB_URI || MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
